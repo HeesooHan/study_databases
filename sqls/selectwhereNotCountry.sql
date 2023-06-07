1c9e7b27c35c24176537e5788d048a58dbef5c44
HeesooHan <hhs950824@gmail.com>
2023-06-07 15:48:31 +0900

    2023-06-07

--- sqls/selectwhereNotCountry.sql
+++ sqls/selectwhereNotCountry.sql
@@ -1,3 +1,7 @@
+-- https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_where
+
 SELECT *
 FROM Customers
 WHERE Country NOT IN ('Germany', 'USA') ;
+
+-- Result : 67
---
+++ sqls/selectwhereRangeAndEquals.sql
@@ -0,0 +1,7 @@
+--  https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_where
+
+SELECT *
+FROM Customers
+WHERE City = 'London' AND CustomerID BETWEEN 50 AND 89 ;
+
+-- Result : 2
